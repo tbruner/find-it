f4ece5cbdf6c4e41963b631658dd333973b0afba
tbruner <trevor.a.bruner@gmail.com>
2023-02-09 20:27:49 -0500

    Move game image data up to App and check for all items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 //import handleSubmit from "./handles/Handles";
 import Play from "./components/Play";
 import Header from "./components/Header";
+import householdImg from "./assets/find-household.jpg";
 import "./App.css";
 
 function App() {
@@ -20,10 +22,44 @@ function App() {
     </div>
   );
   */
+  const household = {
+    pictureHeight: 700,
+    pictureWidth: 700,
+    image: householdImg,
+    items: [
+      {
+        name: "Rose",
+        image: null,
+        location: [253, 185],
+      },
+      {
+        name: "Globe",
+        image: null,
+        location: [394, 364],
+      },
+      {
+        name: "Candle",
+        image: null,
+        location: [18, 264],
+      },
+      {
+        name: "Paintbrushes",
+        image: null,
+        count: 3,
+        // last paintbrush location has two brushes
+        location: [
+          [62, 611],
+          [564, 563],
+          [464, 84],
+        ],
+      },
+    ],
+  };
+
   return (
     <div id="app">
       <Header />
-      <Play />
+      <Play gameImg={household} />
     </div>
   );
 }
--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,19 +1,7 @@
-import household from "../assets/find-household.jpg";
 import "./Play.css";
 
-const Play = () => {
-  let pictureHeight = 700;
-  let pictureWidth = 700;
+const Play = ({ gameImg }) => {
   let newClick = true;
-  let roseLoc = [253, 185];
-  let globeLoc = [394, 364];
-  let candleLoc = [18, 264];
-  // last paintbrush location has two brushes
-  let paintbrushLocs = [
-    [62, 611],
-    [564, 563],
-    [464, 84],
-  ];
 
   function checkClick(e) {
     const targetBox = document.querySelector("#target-container");
@@ -24,30 +12,52 @@ const Play = () => {
 
       let cursorX = e.clientX - rect.left;
       let cursorY = e.clientY - rect.top;
+
       let left = cursorX - 35;
       if (left < 0) left = 0;
-      if (left > pictureWidth - 70) left = pictureWidth - 70;
+      if (left > gameImg.pictureWidth - 70) left = gameImg.pictureWidth - 70;
+
       let top = cursorY - 35;
       if (top < 0) top = 0;
-      if (top > pictureHeight - 70) top = pictureHeight - 70;
+      if (top > gameImg.pictureHeight - 70) top = gameImg.pictureHeight - 70;
 
       targetBox.style.left = left + "px";
       targetBox.style.top = top + "px";
       targetBox.style.display = "block";
 
-      if (
-        roseLoc[0] > left &&
-        roseLoc[0] < left + 70 &&
-        roseLoc[1] > top &&
-        roseLoc[1] < top + 70
-      ) {
-        dropdown.style.backgroundColor = "white";
-      } else {
+      let found = false;
+      gameImg.items.forEach((item) => {
+        if (isNaN(item.location[0])) {
+          item.location.forEach((loc) => {
+            if (
+              loc[0] > left &&
+              loc[0] < left + 70 &&
+              loc[1] > top &&
+              loc[1] < top + 70
+            ) {
+              dropdown.style.backgroundColor = "white";
+              found = true;
+            }
+          });
+        } else if (
+          item.location[0] > left &&
+          item.location[0] < left + 70 &&
+          item.location[1] > top &&
+          item.location[1] < top + 70
+        ) {
+          dropdown.style.backgroundColor = "white";
+          found = true;
+        }
+      });
+
+      if (!found) {
         dropdown.style.backgroundColor = "black";
       }
+
       newClick = false;
     } else {
       targetBox.style.display = "none";
+
       newClick = true;
     }
   }
@@ -56,7 +66,7 @@ const Play = () => {
     <div id="game-space">
       <img
         className="game-img"
-        src={household}
+        src={gameImg.image}
         alt="household hidden object image"
         onClick={checkClick}
       />
